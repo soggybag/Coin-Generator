cb25fdfb3a99d58fe975d3f35726a68c02fba1b0
Mitchell Hudson <mitchellhudson@mitchells-mbp.gateway.sonic.net>
2016-07-25 19:34:43 -0700

    update
    Added comments

--- Coin Generator/GameScene.swift
+++ Coin Generator/GameScene.swift
@@ -6,30 +6,59 @@
 //  Copyright (c) 2016 mitchell hudson. All rights reserved.
 //
 
+
+
+/*
+    Generate coins in shaped blocks. Define a block as a nested array. 
+    Think of the this array as describing the position of coins in 
+    rows and columns. A 1 in the columns array places a coin, while a
+    0 means no coin. 
+ 
+    Coins are animated with a array textures from the coin.atlas. 
+ 
+*/
+
+
+
+
 import SpriteKit
 
 class GameScene: SKScene {
     
+    var redBox: SKSpriteNode!
+    
+    // Define the size for coins.
     let coinSize = CGSize(width: 16, height: 16)
+    // Define an array of textures. This is filled with text s in didMoveToView.
     var coinTextures = [SKTexture]()
     
+    // Define a block of coins. This block creates the letter M.
     let coinBlock = [[1,1,0,0,0,1,1],
                      [1,1,1,0,1,1,1],
                      [1,1,1,1,1,1,1],
                      [1,1,0,1,0,1,1],
                      [1,1,0,0,0,1,1]]
     
+    
+    // A utility to shift elements in the textures array by one.
+    func shift(inout array:[SKTexture]) {
+        array.append(array.removeFirst())
+    }
+    
+    // Make a coin. we'll call this each time we need a coin.
     func makeCoin() -> SKNode {
-        
         let coin = SKSpriteNode(color: UIColor.yellowColor(), size: coinSize)
         
         let animate = SKAction.animateWithTextures(coinTextures, timePerFrame: 0.2, resize: true, restore: false)
         let forever = SKAction.repeatActionForever(animate)
         coin.runAction(forever)
         
+        shift(&coinTextures)
+        
         return coin
     }
     
+    // Make a block of coins. This returns a node containing all coins.
     func makeCoinBlock() -> SKNode {
         let coinNode = SKNode()
         for row in 0 ..< coinBlock.count {
@@ -38,6 +67,7 @@ class GameScene: SKScene {
                     let coin = makeCoin()
                     coinNode.addChild(coin)
                     coin.position.x = CGFloat(col) * coinSize.width
+                    // Row needs to be inverted!
                     coin.position.y = CGFloat(-row) * coinSize.height
                 }
             }
@@ -47,11 +77,20 @@ class GameScene: SKScene {
     }
     
     override func didMoveToView(view: SKView) {
+        
+        redBox = SKSpriteNode(color: UIColor.redColor(), size: CGSize(width: 40, height: 40))
+        addChild(redBox)
+        redBox.position.x = 200
+        redBox.position.y = 400
+        
+        // Setup the textures array.
         for i in 1...5 {
             coinTextures.append(SKTexture(imageNamed: "Coin_\(i)"))
         }
         
+        // Make a block of coins.
         let block = makeCoinBlock()
+        // Add and position block of coins.
         addChild(block)
         block.position.x = 100
         block.position.y = 300
@@ -59,13 +98,15 @@ class GameScene: SKScene {
     
     override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?) {
        /* Called when a touch begins */
-        
-        for touch in touches {
-            
-        }
+       
     }
    
     override func update(currentTime: CFTimeInterval) {
         /* Called before each frame is rendered */
+        
     }
 }
+
+
+
+
